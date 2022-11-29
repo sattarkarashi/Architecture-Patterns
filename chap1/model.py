63bf3ec3d80702b4c5660528a4f761310b7e0f64
sattarkarashi <satokarashi@gmail.com>
2022-11-29 01:30:11 +0100

    chapter one in progress

---
+++ chap1/model.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qt: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.avaliable_quantity = qty
+
+    def allocate(self, line: OrderLine):
+        self.avaliable_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
