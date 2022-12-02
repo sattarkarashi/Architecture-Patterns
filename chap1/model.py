c2e924addc041c1779ddf6c85961c9130df7f50e
sattarkarashi <satokarashi@gmai.com>
2022-12-02 15:46:30 +0100

    added more code to the TDD process

--- chap1/model.py
+++ chap1/model.py
@@ -11,14 +11,30 @@ class OrderLine:
 
 
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+    def __init__(
+        self, ref: str, sku: str, qty: int, eta: Optional[date]
+    ):
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.avaliable_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
     def allocate(self, line: OrderLine):
-        self.avaliable_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
